1bbd5ff7f84c1a2a51c0d10a38a394e1edc34966
Мальцева Евгения <malceva13205@mail.ru>
2021-02-24 15:28:03 +0700

    ThirdCommit
    Not pixel-perfec but still good for me ;)

--- js/index.js
+++ js/index.js
@@ -1,9 +1,13 @@
+/*--------------------------------SLIDER--------------------------------*/
+
 $('.slider').slick({
   fade: true,
-	infinite: true,
-	dots: true,
-	slidesToShow: 1
+  infinite: true,
+  dots: true,
+  slidesToShow: 1
 });
+
+/*--------------------------------CHANGING TEXT IN TITLE--------------------------------*/
 
 var typed = new Typed('.element', {
   strings: ["future.", "startup.", "success."],
@@ -12,4 +16,53 @@ var typed = new Typed('.element', {
   backDelay: 500,
   loop: true,
   showCursor: true
+});
+
+/*--------------------------------FANCYBOX--------------------------------*/
+
+$(document).ready(function() {
+  $(".fancybox").fancybox();
+});
+
+/*--------------------------------POPPER--------------------------------*/
+
+var ref = $('.landing-button');
+var popup = $('.landings-popper');
+var title = $('.title');
+popup.hide();
+
+ref.mouseenter(function() {
+  popup.show();
+  var popper = new Popper(ref, popup, {
+    placement: 'bottom',
+    modifiers: {
+      flip: {
+        behavior: ['left', 'right', 'top', 'bottom']
+      },
+      offset: {
+        enabled: true,
+        offset: 0.1
+      }
+    }
+  });
+});
+
+ref.click(function() {
+  popup.toggle(ref);
+  var popper = new Popper(ref, popup, {
+    placement: 'bottom',
+    modifiers: {
+      flip: {
+        behavior: ['left', 'right', 'top', 'bottom']
+      },
+      offset: {
+        enabled: true,
+        offset: 0.1
+      }
+    }
+  });
+});
+
+title.mouseenter(function() {
+  popup.hide();
 });
